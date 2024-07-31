5a0261f6d7324eed5fed9364a037338838406954
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-07-31 23:29:27 +0600

    optimized image added

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import blogBanner from '../../../public/blog.jpg'
 
 const page = () => {
     const blogs = [
@@ -20,18 +22,31 @@ const page = () => {
         },
     ]
     return (
-        <div className='min-h-screen container mx-auto pt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {
-                blogs?.map(blog => 
-                    <Link 
-                    key={blog?.id}
-                    href={`/blogs/${blog?.id}`}
-                    className='h-[100px] border border-gray-500 bg-stone-300 rounded-lg  text-2xl flex items-center justify-center cursor-pointer hover:scale-105 duration-500'
-                    >
-                        {blog?.title}
-                    </Link>
-                )
-            }
+        <div className='min-h-screen container mx-auto pt-12 space-y-12'>
+
+            <h1 className='text-center text-3xl '>Blogs</h1>
+            <Image
+                src={blogBanner}
+                alt='Blog Banner'
+                placeholder='blur'
+                className='h-[300px] object-cover rounded-lg'
+            ></Image>
+
+            <div className='mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    blogs?.map(blog =>
+                        <Link
+                            key={blog?.id}
+                            href={`/blogs/${blog?.id}`}
+                            className='h-[100px] border border-gray-500 bg-stone-300 rounded-lg  text-2xl flex items-center justify-center cursor-pointer hover:scale-105 duration-500'
+                        >
+                            {blog?.title}
+                        </Link>
+                    )
+                }
+            </div>
+
+
         </div>
     );
 };
