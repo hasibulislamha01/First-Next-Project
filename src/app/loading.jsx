1868ac6b4b50af061e8fb54875a8995dd40c7650
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-08-01 22:51:48 +0600

    loading page added

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='min-h-screen flex justify-center items-center '>
+            <span className="loading loading-spinner text-accent text-3xl font-semibold"></span>
+            <h1 className='text-center text-lg'>...Loading...</h1>
+        </div>
+    );
+};
+
+export default loading;
